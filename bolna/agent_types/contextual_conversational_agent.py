313de6c0c363ca65eeff9a5e4722817b50f43e40
Codemonk-adi <adityachhitarka2001@gmail.com>
2024-08-05 08:49:18 +0530

    changed model

--- bolna/agent_types/contextual_conversational_agent.py
+++ bolna/agent_types/contextual_conversational_agent.py
@@ -15,7 +15,7 @@ class StreamingContextualAgent(BaseAgent):
     def __init__(self, llm):
         super().__init__()
         self.llm = llm
-        self.conversation_completion_llm = OpenAiLLM(model=os.getenv('CHECK_FOR_COMPLETION_LLM', llm.model))
+        self.conversation_completion_llm = llm
         self.history = [{'content': ""}]
 
     async def check_for_completion(self, messages, check_for_completion_prompt = CHECK_FOR_COMPLETION_PROMPT):
